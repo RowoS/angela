0d9402ed704f1c5a2270a06f802b6b82f993fb1e
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2026-08-04 08:36:45 +0800

    feat: add role-based menu item rendering in sidebar

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,22 +1,43 @@
 import React from "react"
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/AppSidebar"
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+import type { MenuItem } from "@/lib/types/sidebar";
 import { ADMIN_MENU, AGENT_MENU, MANAGER_MENU } from '@/config/navigation';
 import type { UserRole } from "@/lib/types/sidebar"
 
-export default function AuthenticatedLayout({ children }: { children: React.ReactNode }) {
-    // TODO(Backend): To fetch user role
-    console.log("root layout auth!")
+export default async function AuthenticatedLayout({ children }: { children: React.ReactNode }) {
 
-    const userRole: UserRole = "admin"
+   console.log("root layout auth!")
 
-    // Pick the right navigation menu for this user's role
-    const currentMenuItems =
-        userRole === "admin"
-            ? ADMIN_MENU
-            : userRole === "manager"
-            ? MANAGER_MENU
-            : AGENT_MENU
+  const supabase = await createClient()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+  if (!user) redirect('/login')
+
+  const { data: profile } = await supabase
+    .from('profiles')
+    .select('role')
+    .eq('id', user.id)
+    .single()
+
+
+  let currentMenuItems = [] as MenuItem[]
+
+  if (profile?.role === 'admin')
+  {
+    currentMenuItems = ADMIN_MENU;
+  }
+  else if (profile?.role === 'manager')
+  {
+    currentMenuItems = MANAGER_MENU;
+  }
+  else if (profile?.role === 'agent')
+  {
+    currentMenuItems = AGENT_MENU;
+  }
 
     return (
         <div className="bg-[#F8F8F8] w-full min-h-screen">
